ff3805d405f87166fb759b5f4122f204b771036d
Documenter.jl <documenter@juliadocs.github.io>
2024-06-21 21:14:01 +0000

    build based on 23ad792

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = VicsekModel","category":"page"},{"location":"#VicsekModel","page":"Home","title":"VicsekModel","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for VicsekModel.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [VicsekModel]","category":"page"}]
+}
